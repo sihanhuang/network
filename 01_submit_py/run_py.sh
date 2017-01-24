e403145527a93fb3adbe741f6174eb15e4c7eafa
Yuanjun Gao <yg2312@holmes.cm.cluster>
2017-01-24 14:12:19 -0500

    minor update

--- 01_submit_py/run_py.sh
+++ 01_submit_py/run_py.sh
@@ -2,9 +2,9 @@
 #
 #
 #SBATCH --account=stats      # The account name for the job.
-#SBATCH --job-name=train_GPU    # The job name.
+#SBATCH --job-name=run_py    # The job name.
 #SBATCH -c 1                     # The number of cpu cores to use.
-#SBATCH --time=10:00:00              # The time the job will take to run.
+#SBATCH --time=1:00:00              # The time the job will take to run.
 #SBATCH --mem-per-cpu=5gb        # The memory the job will use per cpu core.
 
 python test_submit.py -n 5
--- 02_submit_jobarray/run_py_array.sh
+++ 02_submit_jobarray/run_py_array.sh
@@ -2,7 +2,7 @@
 #
 #
 #SBATCH --account=stats      # The account name for the job.
-#SBATCH --job-name=train_GPU    # The job name.
+#SBATCH --job-name=run_py_array    # The job name.
 #SBATCH --array=1-5            # submit 16 jobs with SLURM_ARRAY_TASK_ID ranging from 1 to 5
 #SBATCH -c 1                     # The number of cpu cores to use.
 #SBATCH --time=1:00:00              # The time the job will take to run.
